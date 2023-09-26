0f944dabeecab3dad66e596ce092555592f0f30b
aestheticviny <116309506+aestheticviny@users.noreply.github.com>
2023-09-25 21:26:04 -0300

    Update listaExercicios02.sql

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -7,3 +7,31 @@ DELIMITER ;
 
 -- Teste da stored procedure
 CALL sp_ListarAutores();
+
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END //
+DELIMITER ;
+
+-- Teste da stored procedure
+CALL sp_LivrosPorCategoria('Romance');
+
+DELIMITER //
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN categoriaNome VARCHAR(100), OUT total INT)
+BEGIN
+    SELECT COUNT(*) INTO total
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END //
+DELIMITER ;
+
+-- Teste da stored procedure
+SET @total := 0;
+CALL sp_ContarLivrosPorCategoria('Ciência', @total);
+SELECT @total;
