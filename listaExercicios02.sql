a59a2f54fea4e9ac0abb3ef0f6d831c390bdae59
aestheticviny <116309506+aestheticviny@users.noreply.github.com>
2023-09-25 21:26:36 -0300

    Update listaExercicios02.sql

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -35,3 +35,60 @@ DELIMITER ;
 SET @total := 0;
 CALL sp_ContarLivrosPorCategoria('Ciência', @total);
 SELECT @total;
+
+DELIMITER //
+CREATE PROCEDURE sp_VerificarLivrosCategoria(IN categoriaNome VARCHAR(100), OUT temLivros BOOL)
+BEGIN
+    DECLARE total INT;
+    CALL sp_ContarLivrosPorCategoria(categoriaNome, total);
+    IF total > 0 THEN
+        SET temLivros := TRUE;
+    ELSE
+        SET temLivros := FALSE;
+    END IF;
+END //
+DELIMITER ;
+
+-- Teste da stored procedure
+SET @temLivros := NULL;
+CALL sp_VerificarLivrosCategoria('História', @temLivros);
+SELECT @temLivros;
+
+DELIMITER //
+CREATE PROCEDURE sp_LivrosAteAno(IN anoPublicacao INT)
+BEGIN
+    SELECT Titulo
+    FROM Livro
+    WHERE Ano_Publicacao <= anoPublicacao;
+END //
+DELIMITER ;
+
+-- Teste da stored procedure
+CALL sp_LivrosAteAno(2010);
+
+DELIMITER //
+CREATE PROCEDURE sp_TitulosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    DECLARE done INT DEFAULT 0;
+    DECLARE livroTitulo VARCHAR(255);
+    DECLARE cur CURSOR FOR
+        SELECT Livro.Titulo
+        FROM Livro
+        INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+        WHERE Categoria.Nome = categoriaNome;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+    OPEN cur;
+    read_loop: LOOP
+        FETCH cur INTO livroTitulo;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+        SELECT livroTitulo;
+    END LOOP;
+    CLOSE cur;
+END //
+DELIMITER ;
+
+-- Teste da stored procedure
+CALL sp_TitulosPorCategoria('Ficção Científica');
